39b45febfb5175aad13441f5ef56c9249b626513
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-06-02 20:38:51 -0400

    Move generated header to build directory.

--- source/InputDirectoryConfig.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace config
-{
-static const char* const input_file_path = "/home/jon/Projects/advent-of-code-cpp/inputs";
-static const char* const solutions_path = "/home/jon/Projects/advent-of-code-cpp/solutions";
-}  // namespace config
